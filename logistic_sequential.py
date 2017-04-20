a0394c43882dd30dc44df4100d551fb95a0a8711
christina <christina.bogdan@gmail.com>
2017-04-20 17:48:12 -0400

    fix sampling

--- logistic_sequential.py
+++ logistic_sequential.py
@@ -9,11 +9,13 @@ class LogisticRegression(LinearClassifierMixin):
     """
     Logistic Regression using Sequential Monte Carlo.
     """
-    def __init__(self, scale=1.0, n_iter=20000, random_state=None, prior_scale=0.1):
+    def __init__(self, scale=1.0, n_iter=20000, random_state=None, prior_scale=10):
         self.scale = scale
         self.n_iter = n_iter
         self.random_state = random_state
         self.prior_scale = prior_scale
+        self.rng_ = check_random_state(random_state)
+
 
     def logistic_function(self, X, w):
         lin = np.matmul(X,w[:-1])+w[-1]
@@ -30,19 +32,17 @@ class LogisticRegression(LinearClassifierMixin):
         coef = np.mean(np.repeat(w_samples_t, resamped, axis=0), axis=0)
         return coef
 
-    def partial_fit(self, X, y):
-        X, y = check_X_y(X, y)
-        n_features = X.shape[1] + 1
+    def partial_fit(self, X=None, y=None, n_features=10):
 
         # Called first time
-        if not hasattr(self, "time_"):
+        if X is None:
             self.rng_ = check_random_state(self.random_state)
             self.w_ = self.rng_.multivariate_normal(
                 np.zeros(n_features),
                 self.prior_scale * np.eye(n_features), size=self.n_iter)
-            self.time_ = 1
         else:
-            self.time_ += 1
+            X, y = check_X_y(X, y)
+            n_features = X.shape[1] + 1
             samples = np.zeros((self.n_iter, n_features))
             cov = self.scale * np.eye(n_features)
             weights = np.zeros(self.n_iter)
@@ -51,8 +51,11 @@ class LogisticRegression(LinearClassifierMixin):
                 samples[i] = self.rng_.multivariate_normal(self.w_[i], cov)
                 weights[i] = -log_loss(y, self.logistic_function(X, samples[i]))
             self.weights_ = self.softmax(weights)
-            self.w_ = samples[self.rng_.multinomial(self.n_iter, self.weights_)]
-
+            self.w_ = samples[np.repeat(np.arange(self.n_iter), \
+                    np.random.multinomial(self.n_iter,self.weights_))]
+        coefs = np.mean(self.w_, axis=0)
+        self.coef_ = coefs[: n_features-1]
+        self.intercept_ = coefs[-1]
 
     def fit(self, X, y, t):
         self.rng = check_random_state(self.random_state)
@@ -84,5 +87,7 @@ class LogisticRegression(LinearClassifierMixin):
         self.intercept_ = coef[:,-1]
 
     def predict(self, X):
-        return np.array(np.sum(np.multiply(self.coef_[self.rev_index],X),axis=1) +\
-         self.intercept_[self.rev_index] > 0,dtype=np.int)
+        return (np.dot(X, self.coef_) + self.intercept_ > 0).astype(np.int)
+        # self.w_
+        # return np.array(np.sum(np.multiply(self.coef_[self.rev_index],X),axis=1) +\
+        #  self.intercept_[self.rev_index] > 0,dtype=np.int)
