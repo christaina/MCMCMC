1e851f5e9e90ee708ff678be8d27b83d621adeeb
MechCoder <manojkumarsivaraj334@gmail.com>
2017-04-24 13:11:14 -0400

    Cosmit

--- logistic_sequential.py
+++ logistic_sequential.py
@@ -9,14 +9,15 @@ class LogisticRegression(LinearClassifierMixin):
     """
     Logistic Regression using Sequential Monte Carlo.
     """
-    def __init__(self, scale=1.0, n_iter=20000, random_state=None, prior_scale=10):
+    def __init__(self, scale=1.0, n_iter=20000, random_state=None,
+                 prior_scale=10.0):
         self.scale = scale
         self.n_iter = n_iter
         self.random_state = random_state
         self.prior_scale = prior_scale
 
     def logistic_function(self, X, w):
-        lin = np.matmul(X,w[:-1])+w[-1]
+        lin = np.matmul(X,w[:-1]) + w[-1]
         return 1./ (1. + np.exp(-lin))
 
     def softmax(self, vec):
@@ -25,23 +26,20 @@ class LogisticRegression(LinearClassifierMixin):
         xform /= np.sum(xform)
         return xform
 
-    def adjust_samples(self,weight_t,w_samples_t):
-        resamped = self.rng.multinomial(self.n_iter,weight_t)
-        coef = np.mean(np.repeat(w_samples_t, resamped, axis=0), axis=0)
-        return coef
-
     def partial_fit(self, X=None, y=None, labels=None, n_features=10):
-        if labels is None:
-            labels = np.unique(y)
-
         # Called first time
         if X is None:
+            if labels is not None:
+                self.labels_ = labels
             self.rng_ = check_random_state(self.random_state)
             self.w_ = self.rng_.multivariate_normal(
                 np.zeros(n_features),
                 self.prior_scale * np.eye(n_features), size=self.n_iter)
         else:
             X, y = check_X_y(X, y)
+
+            if not hasattr(self, "labels_"):
+                self.labels_ = np.unique(y)
             n_features = X.shape[1] + 1
             samples = np.zeros((self.n_iter, n_features))
             cov = self.scale * np.eye(n_features)
@@ -49,11 +47,11 @@ class LogisticRegression(LinearClassifierMixin):
 
             for i in range(self.n_iter):
                 samples[i] = self.rng_.multivariate_normal(self.w_[i], cov)
-                weights[i] = -log_loss(
-                    y, self.logistic_function(X, samples[i]), labels=labels)
+                log_func = self.logistic_function(X, samples[i])
+                weights[i] = -log_loss(y, log_func, labels=self.labels_)
             self.weights_ = self.softmax(weights)
             self.w_ = samples[np.repeat(np.arange(self.n_iter), \
-                    np.random.multinomial(self.n_iter,self.weights_))]
+                    self.rng_.multinomial(self.n_iter,self.weights_))]
         coefs = np.mean(self.w_, axis=0)
         self.coef_ = coefs[: n_features-1]
         self.intercept_ = coefs[-1]
